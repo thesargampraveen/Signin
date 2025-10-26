75851d29b4af840f217b591405663e9bf5572d51
thesargampraveen <sargampraveen3@gmail.com>
2025-10-26 13:55:21 +0530

    task done

--- src/Navigation/Stacknavigation.js
+++ src/Navigation/Stacknavigation.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import {  createNativeStackNavigator } from '@react-navigation/native-stack';
+import auth from '@react-native-firebase/auth';
 import Loginscreen from '../Screen/Loginscreen';
 import Registerscreen from '../Screen/Registerscreen';
 import Home from '../Screen/Home';
 import TodoScreen from '../Screen/TodoScreen';
+
 const Stack = createNativeStackNavigator();
 
 const  Stacknavigation = () => {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) {
+    // You can return a loading screen here if you want
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Todos"
+        initialRouteName={user ? "Todos" : "Register"}
         screenOptions={{
           headerShown: false, // Hide header globally
         }}
